86d44d87d95db5e036b23d488ddda6132b518a01
ImmerxSoftware <62947080+ImmerxSoftware@users.noreply.github.com>
2022-07-13 00:09:14 -0500

    removed test comment

--- src/main/java/net/immerxsw/forgerymod/ForgeryMod.java
+++ src/main/java/net/immerxsw/forgerymod/ForgeryMod.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// comment
 public class ForgeryMod implements ModInitializer {
 	public static final String MOD_ID = "forgerymod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
