fbcc050a9c6d646d592f3a49662234647bdb8348
ImmerxSoftware <62947080+ImmerxSoftware@users.noreply.github.com>
2022-07-13 00:05:34 -0500

    Initial commit

---
+++ src/main/java/net/immerxsw/forgerymod/ForgeryMod.java
@@ -0,0 +1,14 @@
+package net.immerxsw.forgerymod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ForgeryMod implements ModInitializer {
+	public static final String MOD_ID = "forgerymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/immerxsw/forgerymod/ForgeryModClient.java
@@ -0,0 +1,10 @@
+package net.immerxsw.forgerymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForgeryModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
